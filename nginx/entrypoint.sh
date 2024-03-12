3f3822bbc978dd6434c72714967955b1f62a363a
William Eckerleben <ing@williameckerleben.com>
2024-03-12 14:02:07 -0300

    fix: Adjust entrypoint script to modify the correct NGINX config file
       - Updated the sed command in entrypoint.sh to replace the listening port in /etc/nginx/nginx.conf instead of /etc/nginx/conf.d/default.conf. This change ensures that the dynamic port assignment through the  variable is applied to the primary NGINX configuration file, aligning with the actual location of the listen directive in the NGINX setup for our Heroku deployment environment.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # Replaces the port in the NGINX configuration with the value of $PORT
-sed -i 's/listen 80;/listen '"$PORT"';/g' /etc/nginx/conf.d/default.conf
+sed -i 's/listen 80;/listen '"$PORT"';/g' /etc/nginx/nginx.conf
 
 # Execute the original Docker entrypoint command (this maintains the original functionality of the NGINX image).
 exec "$@"
